b69382833f450e7c50a8c1f724215b80be169641
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-06-09 11:06:56 -0500

    basic ga done

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+// using AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using TechJobsMVCAutograded6.Data;
 using TechJobsMVCAutograded6.Models;
@@ -18,5 +19,23 @@ public class SearchController : Controller
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+        // List<Job> jobs = new();
+        if (searchType == "all" && searchTerm == null)
+        {
+            // jobs = JobData.FindAll();
+            // ViewBag.jobs = jobs;
+            ViewBag.jobs = JobData.FindAll();
+        }
+        else
+        {
+            // jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            // ViewBag.jobs = jobs;
+            ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+        }
+        ViewBag.columns = ListController.ColumnChoices;
+        return View("Index");
+    }
 }
 
