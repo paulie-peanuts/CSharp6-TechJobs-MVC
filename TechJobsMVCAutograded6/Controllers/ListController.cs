ff4c56d553ecba5e07f1a437c50949fb8f2367b6
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-06-08 23:24:18 -0500

    task 2 done

--- TechJobsMVCAutograded6/Controllers/ListController.cs
+++ TechJobsMVCAutograded6/Controllers/ListController.cs
@@ -44,6 +44,16 @@ public class ListController : Controller
     // TODO #2 - Complete the Jobs action method
     public IActionResult Jobs(string column, string value)
     {
+        if (column == "All" && value == "View All")
+        {
+            ViewBag.title = value;
+            ViewBag.jobs = JobData.FindAll();
+        }
+        else
+        {
+            ViewBag.title = column + ": " + value;
+            ViewBag.jobs = JobData.FindByColumnAndValue(column, value);
+        }
         return View();
     }
 }
